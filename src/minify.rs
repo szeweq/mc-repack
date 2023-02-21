eb56036f3d003f8de9a2aff526518b941c145268
Szewek <shevkpl@gmail.com>
2023-02-22 00:13:33 +0100

    Sorted file types

--- src/minify.rs
+++ src/minify.rs
@@ -19,7 +19,7 @@ fn find_brackets(b: &[u8]) -> Result<(usize, usize), Box<dyn Error>> {
     Ok((i, j))
 }
 
-const DUMMIES: &[&str] = &["fsh", "vsh", "js", "txt", "html", "glsl", "vert", "nbt", "ogg", "xml", "md"];
+const DUMMIES: &[&str] = &["fsh", "glsl", "html", "js", "md", "nbt", "ogg", "txt", "vert", "vsh", "xml"];
 
 pub fn all_minifiers() -> HashMap<&'static str, Box<dyn Minifier>> {
     let mut popts = oxipng::Options::default();
