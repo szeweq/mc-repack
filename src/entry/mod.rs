74d3a9af83ebd8b577aab7d1f45a0dde020a1814
Szewek <shevkpl@gmail.com>
2023-04-01 00:04:38 +0200

    Use functions as entry readers and savers

--- src/entry/mod.rs
+++ src/entry/mod.rs
@@ -10,6 +10,7 @@ use crate::{optimizer::{EntryType, ProgressState}, errors::ErrorCollector};
 use crossbeam_channel::{Sender, Receiver};
 
 /// Trait for reading entries for further optimization. Typically used with `EntrySaver`.
+/// Any function that matches these arguments (excluding self) can be used as custom entry reader.
 pub trait EntryReader {
     /// Reads entries and sends them via `tx`.
     /// The `use_blacklist` parameter is used to ignore predefined file types written in `blacklist` module.
@@ -21,6 +22,7 @@ pub trait EntryReader {
 }
 
 /// Trait for saving entries that have been optimizer. Typically used with `EntryReader`.
+/// Any function that matches these arguments (excluding self) can be used as custom entry saver.
 pub trait EntrySaver {
     /// Receives entries from `rx`, optimizes, sends progress (via `ps`), and saves them.
     /// Errors are collected with entry names.
@@ -30,4 +32,24 @@ pub trait EntrySaver {
         ev: &mut dyn ErrorCollector,
         ps: &Sender<ProgressState>
     ) -> io::Result<i64>;
+}
+
+impl<T: FnOnce(Sender<EntryType>, bool) -> io::Result<()>> EntryReader for T {
+    fn read_entries(
+        self,
+        tx: Sender<EntryType>,
+        use_blacklist: bool
+    ) -> io::Result<()> {
+        self(tx, use_blacklist)
+    }
+}
+impl <T: FnOnce(Receiver<EntryType>, &mut dyn ErrorCollector, &Sender<ProgressState>) -> io::Result<i64>> EntrySaver for T {
+    fn save_entries(
+        self,
+        rx: Receiver<EntryType>,
+        ev: &mut dyn ErrorCollector,
+        ps: &Sender<ProgressState>
+    ) -> io::Result<i64> {
+        self(rx, ev, ps)
+    }
 }
