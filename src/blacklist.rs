9c9d3c8d2e9ee107b2f286660185d20a80577c59
Szewek <shevkpl@gmail.com>
2023-04-11 08:49:55 +0200

    Use file types directly in code for compiler optimizations

--- src/blacklist.rs
+++ src/blacklist.rs
@@ -1,10 +1,11 @@
 use std::{error::Error, fmt::Display};
 
-const IGNORE_FILE_TYPES: &[&str] = &["blend", "blend1", "psd"];
-
 /// This method checks if a file extension can be ignored (likely not used).
 pub fn can_ignore_type(s: &str) -> bool {
-    IGNORE_FILE_TYPES.binary_search(&s).is_ok()
+    match s {
+        "blend" | "blend1" | "psd" => true,
+        _ => false
+    }
 }
 
 /// An error struct informing that processed file is blacklisted.
@@ -14,6 +15,6 @@ pub struct BlacklistedFile;
 impl Error for BlacklistedFile {}
 impl Display for BlacklistedFile {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Blacklisted file")
+        write!(f, "Blacklisted")
     }
 }
