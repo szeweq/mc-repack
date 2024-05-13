293326cec8f80e4c2affc0f12f89a3c6eb1cc86e
Szewek <shevkpl@gmail.com>
2024-05-13 23:01:23 +0200

    Remove `argh` dependency

--- src/bin/mc-repack/cli_args.rs
+++ src/bin/mc-repack/cli_args.rs
@@ -1,6 +1,5 @@
 use std::path::PathBuf;
 
-#[cfg(not(feature = "argh"))]
 #[derive(Debug, clap::Parser)]
 #[command(version)]
 pub struct Args {
@@ -19,28 +18,7 @@ pub struct Args {
     #[arg(short = 'b', long)]
     pub use_blacklist: bool
 }
-#[cfg(feature = "argh")]
-#[derive(argh::FromArgs)]
-/// Repack jar/zip archives
-pub struct Args {
-    /// (Required) Path to a file/directory of archives (JAR and ZIP)
-    #[argh(positional)]
-    pub path: PathBuf,
 
-    /// (Optional) Destination path. It cannot be the same as the source!
-    #[argh(option)]
-    pub out: Option<PathBuf>,
-
-    /// do not print file errors
-    #[argh(switch)]
-    pub silent: bool,
-
-    /// use built-in blacklist for files
-    #[argh(short = 'b', switch)]
-    pub use_blacklist: bool
-}
-
-#[cfg(not(feature = "argh"))]
 impl Args {
     #[inline]
     pub fn env() -> Self {
@@ -48,14 +26,6 @@ impl Args {
         Self::parse()
     }
 }
-#[cfg(feature = "argh")]
-impl Args {
-    #[inline]
-    pub fn env() -> Self {
-        argh::from_env()
-    }
-}
-
 pub struct RepackOpts {
     pub silent: bool,
     pub use_blacklist: bool
