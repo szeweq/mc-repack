ba316f7ad1196def4ce71e3fee92100ac8901efb
Szewek <shevkpl@gmail.com>
2024-05-14 21:49:02 +0200

    Add Serialize for png config

--- lib-core/src/min/png.rs
+++ lib-core/src/min/png.rs
@@ -1,7 +1,8 @@
 #![cfg(feature = "png")]
 
-use crate::cfg::{acfg, ConfigHolder};
+use state::InitCell;
 
+use crate::cfg::{acfg, ConfigHolder};
 use super::Result_;
 
 #[cfg(feature = "png-zopfli")]
@@ -12,39 +13,42 @@ const BEST_DEFLATE: oxipng::Deflaters = oxipng::Deflaters::Libdeflater { compres
 acfg!(MinifierPNG: PNGConfig);
 impl ConfigHolder<MinifierPNG> {
     pub(super) fn minify(&self, b: &[u8], vout: &mut Vec<u8>) -> Result_ {
-        let v = oxipng::optimize_from_memory(b, &self.oxipng_opts)?;
+        let v = oxipng::optimize_from_memory(b, self.png_opts())?;
         let _ = std::mem::replace(vout, v);
         Ok(())
     }
 }
 
 /// Configuration for PNG optimizer
+#[cfg_attr(feature = "serde-cfg", derive(serde::Serialize, serde::Deserialize))]
 pub struct PNGConfig {
-    oxipng_opts: oxipng::Options,
+    #[cfg_attr(feature = "serde-cfg", serde(skip))]
+    oxipng_opts: InitCell<oxipng::Options>,
     #[cfg(feature = "png-zopfli")]
     use_zopfli: bool
 }
 impl Default for PNGConfig {
     fn default() -> Self {
-        let mut popts = oxipng::Options {
-            fix_errors: true,
-            strip: oxipng::StripChunks::Safe,
-            optimize_alpha: true,
-            deflate: BEST_DEFLATE,
-            ..Default::default()
-        };
-        popts.filter.insert(oxipng::RowFilter::Up);
-        popts.filter.insert(oxipng::RowFilter::Paeth);
-        
-        Self { oxipng_opts: popts, #[cfg(feature = "png-zopfli")] use_zopfli: false }
+        Self { oxipng_opts: InitCell::new(), #[cfg(feature = "png-zopfli")] use_zopfli: false }
     }
 }
 impl PNGConfig {
-    #[cfg(feature = "png-zopfli")]
-    /// Sets whether to use Zopfli for PNG compression
-    pub fn use_zopfli(&mut self, v: bool) -> &mut Self {
-        self.use_zopfli = v;
-        self.oxipng_opts.deflate = if v { BEST_ZOPFLI } else { BEST_DEFLATE };
-        self
+    fn png_opts(&self) -> &oxipng::Options {
+        self.oxipng_opts.get_or_init(|| {
+            let mut popts = oxipng::Options {
+                fix_errors: true,
+                strip: oxipng::StripChunks::Safe,
+                optimize_alpha: true,
+                #[cfg(feature = "png-zopfli")]
+                deflate: if self.use_zopfli { BEST_ZOPFLI } else { BEST_DEFLATE },
+                #[cfg(not(feature = "png-zopfli"))]
+                deflate: BEST_DEFLATE,
+                ..Default::default()
+            };
+            popts.filter.insert(oxipng::RowFilter::Up);
+            popts.filter.insert(oxipng::RowFilter::Paeth);
+
+            popts
+        })
     }
 }
