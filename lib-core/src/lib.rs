a7242015b3cfd08bd19476cae781583fceb3f773
Szewek <shevkpl@gmail.com>
2024-05-22 01:57:18 +0200

    Bump to 0.21.0

--- lib-core/src/lib.rs
+++ lib-core/src/lib.rs
@@ -1,9 +1,6 @@
 //! MC-Repack is initially built as a CLI app, but can also be used as a library.
 //! This crate contains methods necessary to work with files that need optimizations.
 //! 
-//! You should be interested in `optimizer` module. There are important methods used for repaching
-//! and optimizing files.
-//! 
 //! This crate considers that the repacked files are used in Minecraft mods. You can still use the library
 //! for other types, like Android or Gradle files.
 
