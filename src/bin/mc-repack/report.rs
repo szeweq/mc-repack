e6c95c2757a3e9146a66d427263304597a51400d
Szewek <shevkpl@gmail.com>
2024-08-21 16:13:29 +0200

    Tidy up the report module

--- src/bin/mc-repack/report.rs
+++ src/bin/mc-repack/report.rs
@@ -1,19 +1,19 @@
-use std::fs;
+use std::{fs, io, path};
 
-pub struct Report(Box<std::path::Path>, Vec<(Box<str>, u64, u64)>);
+pub struct Report(Box<path::Path>, Vec<(Box<str>, u64, u64)>);
 impl Report {
-    pub fn new(p: Box<std::path::Path>) -> Self {
+    pub fn new(p: Box<path::Path>) -> Self {
         Self(p, Vec::new())
     }
     pub fn push(&mut self, name: &str, old_size: u64, new_size: u64) {
         self.1.push((name.into(), old_size, new_size));
     }
-    pub fn save_csv(&self) -> std::io::Result<()> {
-        use std::io::Write;
-        fn write_impl(w: &mut fs::File, v: &[(Box<str>, u64, u64)]) -> std::io::Result<()> {
+    pub fn save_csv(&self) -> io::Result<()> {
+        use io::Write;
+        fn write_impl(w: &mut fs::File, v: &[(Box<str>, u64, u64)]) -> io::Result<()> {
             writeln!(w, "name,old_size,new_size")?;
             for (name, old_size, new_size) in v {
-                writeln!(w, "{},{},{}", name, old_size, new_size)?;
+                writeln!(w, "{name},{old_size},{new_size}")?;
             }
             Ok(())
         }
