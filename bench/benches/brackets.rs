af4cce14b0acb81ab7702aed0660ffe10b447259
Szewek <shevkpl@gmail.com>
2024-10-16 07:38:33 +0200

    New benchmarks for finding brackets

--- bench/benches/brackets.rs
+++ bench/benches/brackets.rs
@@ -5,34 +5,12 @@ const JSON_LIKE_DATA: &[u8] = b"
 ~ ~ ~  \xfe \xfd \xdd == JSON DATA START: ==
 {
     \"a\": 1,
-    \"b\": 2
+    \"b\": 2.0,
+    \"c\": \"c\"
 } \xff\xff\xff
 == JSON DATA END ==
 
 ";
-
-#[inline]
-fn find_brackets(b: &[u8]) -> Option<(usize, usize)> {
-    let (i, endb) = match b.iter().enumerate().find(|(_, &b)| b == b'{' || b == b'[') {
-        Some((i, b'{')) => (i, b'}'),
-        Some((i, b'[')) => (i, b']'),
-        _ => { return None; }
-    };
-    let j = b.iter().rposition(|&b| b == endb)?;
-    Some((i, j))
-}
-
-#[inline]
-fn find_brackets_new(b: &[u8]) -> Option<(usize, usize)> {
-    let i = b.iter().position(|&b| b == b'{' || b == b'[')?;
-    let endb = match b[i] {
-        b'{' => b'}',
-        b'[' => b']',
-        _ => { return None; }
-    };
-    let j = b.iter().rposition(|&b| b == endb)?;
-    Some((i, j))
-}
 
 #[inline]
 fn find_brackets_direct(b: &[u8]) -> Option<&[u8]> {
@@ -57,12 +35,30 @@ fn find_brackets_new_direct(b: &[u8]) -> Option<&[u8]> {
     Some(&b[i..=j])
 }
 
+#[inline]
+fn find_brackets_opt(b: &[u8]) -> Option<&[u8]> {
+    let (i, endb) = match b.iter().enumerate().find(|(_, &b)| b == b'{' || b == b'[') {
+        Some((i, x)) => (i, x ^ 6),
+        _ => { return None; }
+    };
+    let j = b.iter().rposition(|&b| b == endb)?;
+    Some(&b[i..=j])
+}
+
+#[inline]
+fn find_brackets_opt_new(b: &[u8]) -> Option<&[u8]> {
+    let i = b.iter().position(|&b| b == b'{' || b == b'[')?;
+    let endb = b[i] ^ 6;
+    let j = b.iter().rposition(|&b| b == endb)?;
+    Some(&b[i..=j])
+}
+
 pub fn bench_brackets(c: &mut Criterion) {
     let mut g = c.benchmark_group("brackets");
-    g.bench_function("old", |b| b.iter(|| find_brackets(black_box(JSON_LIKE_DATA))));
-    g.bench_function("new", |b| b.iter(|| find_brackets_new(black_box(JSON_LIKE_DATA))));
     g.bench_function("old direct", |b| b.iter(|| find_brackets_direct(black_box(JSON_LIKE_DATA))));
     g.bench_function("new direct", |b| b.iter(|| find_brackets_new_direct(black_box(JSON_LIKE_DATA))));
+    g.bench_function("optimized", |b| b.iter(|| find_brackets_opt(black_box(JSON_LIKE_DATA))));
+    g.bench_function("optimized new", |b| b.iter(|| find_brackets_opt_new(black_box(JSON_LIKE_DATA))));
     g.finish();
 }
 
