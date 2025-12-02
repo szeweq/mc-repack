59bb532b1f240034f8bff1ae043403cf5ed3c291
Szewek <shevkpl@gmail.com>
2025-12-02 02:12:25 +0100

    Fixes for edition transition

--- src/bin/mc-repack/iter.rs
+++ src/bin/mc-repack/iter.rs
@@ -1,23 +1,31 @@
 use std::{io, path::Path};
 
-
 type FileResult = io::Result<(Option<bool>, Box<Path>)>;
 
 pub enum Files {
     Single(Option<FileResult>),
-    Dir(std::vec::IntoIter<FileResult>)
+    Dir(std::vec::IntoIter<FileResult>),
 }
 impl Files {
     pub fn from_path(p: &Path) -> io::Result<(Box<Path>, Self)> {
         let ft = p.metadata()?.file_type();
         let p: Box<Path> = Box::from(p);
         if ft.is_dir() {
-            Ok((p.clone(), Self::Dir(walkdir::WalkDir::new(p).into_iter().map(|r| Ok(check_dir_entry(r?))).collect::<Vec<_>>().into_iter())))
+            Ok((
+                p.clone(),
+                Self::Dir(
+                    walkdir::WalkDir::new(p)
+                        .into_iter()
+                        .map(|r| Ok(check_dir_entry(r?)))
+                        .collect::<Vec<_>>()
+                        .into_iter(),
+                ),
+            ))
         } else if ft.is_file() {
             let parent = p.parent().unwrap();
             Ok((parent.into(), Self::Single(Some(Ok((Some(false), p))))))
         } else {
-            Err(io::Error::new(io::ErrorKind::Other, "Not a file or directory"))
+            Err(io::Error::other("Not a file or directory"))
         }
     }
 }
@@ -26,14 +34,14 @@ impl Iterator for Files {
     fn next(&mut self) -> Option<Self::Item> {
         match self {
             Self::Single(it) => it.take(),
-            Self::Dir(it) => it.next()
+            Self::Dir(it) => it.next(),
         }
     }
     fn size_hint(&self) -> (usize, Option<usize>) {
         match self {
             Self::Single(Some(_)) => (1, Some(1)),
             Self::Single(None) => (0, Some(0)),
-            Self::Dir(it) => it.size_hint()
+            Self::Dir(it) => it.size_hint(),
         }
     }
 }
@@ -42,7 +50,7 @@ impl ExactSizeIterator for Files {
         match self {
             Self::Single(Some(_)) => 1,
             Self::Single(None) => 0,
-            Self::Dir(it) => it.len()
+            Self::Dir(it) => it.len(),
         }
     }
 }
@@ -50,11 +58,14 @@ impl ExactSizeIterator for Files {
 fn check_dir_entry(de: walkdir::DirEntry) -> (Option<bool>, Box<Path>) {
     let ft = de.file_type();
     let p = de.into_path().into_boxed_path();
-    (if ft.is_dir() {
-        Some(true)
-    } else if ft.is_file() {
-        Some(false)
-    } else {
-        None
-    }, p)
+    (
+        if ft.is_dir() {
+            Some(true)
+        } else if ft.is_file() {
+            Some(false)
+        } else {
+            None
+        },
+        p,
+    )
 }
--- src/bin/mc-repack/main.rs
+++ src/bin/mc-repack/main.rs
@@ -1,15 +1,20 @@
-use std::{fs, path::Path, sync::Arc};
 use clap::Parser;
 use cli_args::{Cmd, FilesArgs, JarsArgs, RepackOpts};
 use crossbeam_channel::{Receiver, Sender};
-use indicatif::{ProgressBar, ProgressStyle, MultiProgress};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use iter::Files;
-use mc_repack_core::{cfg, entry::{self, process_entry, read_entry, EntryReader, EntrySaver, NamedEntry, ReadEntryIter}, errors::ErrorCollector, fop::TypeBlacklist, ProgressState};
+use mc_repack_core::{
+    ProgressState, cfg,
+    entry::{self, EntryReader, EntrySaver, NamedEntry, ReadEntryIter, process_entry, read_entry},
+    errors::ErrorCollector,
+    fop::TypeBlacklist,
+};
+use std::{fs, path::Path, sync::Arc};
 
 mod cli_args;
 mod config;
-mod report;
 mod iter;
+mod report;
 
 type Error_ = anyhow::Error;
 type Result_<T> = Result<T, Error_>;
@@ -17,7 +22,7 @@ type Result_<T> = Result<T, Error_>;
 fn main() -> Result_<()> {
     println!(include_str!("intro.txt"));
     let args = cli_args::Args::parse();
-    
+
     match &args.cmd {
         Cmd::Jars(ja) => {
             let path = &ja.path;
@@ -55,13 +60,15 @@ fn main() -> Result_<()> {
 const PB_STYLE_ZIP: &str = "# {pos}/{len} {wide_msg}";
 
 fn file_progress_bar() -> ProgressBar {
-    ProgressBar::new(0).with_style(
-        ProgressStyle::with_template(PB_STYLE_ZIP).unwrap()
-    )
+    ProgressBar::new(0).with_style(ProgressStyle::with_template(PB_STYLE_ZIP).unwrap())
 }
 
 fn process_jars(base: &Path, fit: Files, jargs: &JarsArgs, opts: &mut RepackOpts) -> Result_<()> {
-    let RepackOpts { ref blacklist, ref cfgmap, .. } = opts;
+    let &mut RepackOpts {
+        ref blacklist,
+        ref cfgmap,
+        ..
+    } = opts;
     let ec = &mut opts.err_collect;
     let clvl = 9 + jargs.zopfli.map_or(0, |x| i64::from(x.get()));
     let mp = MultiProgress::new();
@@ -69,23 +76,28 @@ fn process_jars(base: &Path, fit: Files, jargs: &JarsArgs, opts: &mut RepackOpts
     let mut db = fs::DirBuilder::new();
     db.recursive(true);
 
-    let pb = mp.add(ProgressBar::new_spinner().with_style(
-        ProgressStyle::with_template("{wide_msg}").unwrap()
-    ));
+    let pb = mp.add(
+        ProgressBar::new_spinner().with_style(ProgressStyle::with_template("{wide_msg}").unwrap()),
+    );
     let pb2 = mp.add(file_progress_bar());
     let ps = thread_progress_bar(pb2);
 
     for fp in fit {
         let (ftype, fp) = fp?;
         let Some(fname) = fp.file_name() else {
-            return invalid_file_name()
+            return invalid_file_name();
         };
         let Some(relp) = pathdiff::diff_paths(&fp, base) else {
-            return invalid_file_name()
+            return invalid_file_name();
         };
         let relname = relp.to_string_lossy();
         let fname = fname.to_string_lossy();
-        if matches!(ftype, Some(false)) && matches!(relp.extension().map(std::ffi::OsStr::as_encoded_bytes), Some(b"jar" | b"zip")) {
+        if matches!(ftype, Some(false))
+            && matches!(
+                relp.extension().map(std::ffi::OsStr::as_encoded_bytes),
+                Some(b"jar" | b"zip")
+            )
+        {
             ec.rename(&relname);
             pb.set_message(fname.to_string());
             let nfp = jargs.out.join(&relp);
@@ -97,15 +109,18 @@ fn process_jars(base: &Path, fit: Files, jargs: &JarsArgs, opts: &mut RepackOpts
                 &mut entry::ZipEntrySaver::custom_compress(
                     fs::File::create(&nfp)?,
                     jargs.keep_dirs,
-                    clvl
+                    clvl,
                 ),
-                cfgmap, &ps, ec, blacklist.clone()
+                cfgmap,
+                &ps,
+                ec,
+                blacklist.clone(),
             ) {
                 Ok(()) => {
                     if let Some(ref mut report) = opts.report {
                         report_sizes(report, &relname, &fp, &nfp);
                     }
-                },
+                }
                 Err(e) => {
                     println!("Cannot repack {}: {}\n\n", fp.display(), e);
                     if let Err(fe) = fs::remove_file(&nfp) {
@@ -122,14 +137,21 @@ fn process_jars(base: &Path, fit: Files, jargs: &JarsArgs, opts: &mut RepackOpts
 }
 
 fn process_files(base: &Path, fit: Files, fargs: &FilesArgs, opts: &mut RepackOpts) -> Result_<()> {
-    let RepackOpts { ref blacklist, ref cfgmap, .. } = opts;
+    let &mut RepackOpts {
+        ref blacklist,
+        ref cfgmap,
+        ..
+    } = opts;
     let ec = &mut opts.err_collect;
     let pb2 = file_progress_bar();
     let ps = thread_progress_bar(pb2);
     optimize_with(
         &mut entry::FSEntryReader::custom(base.into(), fit),
         &mut entry::FSEntrySaver::new(fargs.out.clone().into_boxed_path()),
-        cfgmap, &ps, ec, blacklist.clone()
+        cfgmap,
+        &ps,
+        ec,
+        blacklist.clone(),
     )?;
     drop(ps);
     Ok(())
@@ -140,7 +162,7 @@ fn files_report(report: &mut report::Report, path: &Path, out: &Path) -> Result_
     for fp in fit {
         let (ftype, fp) = fp?;
         let Some(relp) = pathdiff::diff_paths(&fp, &base) else {
-            return invalid_file_name()
+            return invalid_file_name();
         };
         let relname = relp.to_string_lossy();
         if matches!(ftype, Some(false)) {
@@ -173,7 +195,9 @@ fn thread_progress_bar(pb: ProgressBar) -> Sender<ProgressState> {
     rayon::spawn(move || {
         for st in pr {
             match st {
-                ProgressState::Start(u) => { pb.set_length(u as u64); }
+                ProgressState::Start(u) => {
+                    pb.set_length(u as u64);
+                }
                 ProgressState::Push(num, msg) => {
                     pb.set_position(num as u64);
                     pb.set_message(msg.to_string());
@@ -203,7 +227,7 @@ fn optimize_with<R: EntryReader + Send + 'static, S: EntrySaver + Send + 'static
     cfgmap: &cfg::ConfigMap,
     ps: &Sender<ProgressState>,
     errors: &mut ErrorCollector,
-    blacklist: Arc<TypeBlacklist>
+    blacklist: Arc<TypeBlacklist>,
 ) -> crate::Result_<()> {
     let (tx, rx) = crossbeam_channel::unbounded();
     wrap_send(ps, ProgressState::Start(reader.read_len()))?;
@@ -222,7 +246,11 @@ fn optimize_with<R: EntryReader + Send + 'static, S: EntrySaver + Send + 'static
     }
 }
 
-fn reading<R: EntryReader>(iter: ReadEntryIter<R>, tx: &Sender<NamedEntry>, blacklist: &TypeBlacklist) -> Result_<()> {
+fn reading<R: EntryReader>(
+    iter: ReadEntryIter<R>,
+    tx: &Sender<NamedEntry>,
+    blacklist: &TypeBlacklist,
+) -> Result_<()> {
     for re in iter {
         if let Some(ne) = read_entry::<R>(re, blacklist)? {
             wrap_send(tx, ne)?;
@@ -230,7 +258,13 @@ fn reading<R: EntryReader>(iter: ReadEntryIter<R>, tx: &Sender<NamedEntry>, blac
     }
     Ok(())
 }
-fn saving<S: EntrySaver>(saver: &mut S, rx: Receiver<NamedEntry>, ps: &Sender<ProgressState>, errors: &mut ErrorCollector, cfgmap: &cfg::ConfigMap) -> Result_<()> {
+fn saving<S: EntrySaver>(
+    saver: &mut S,
+    rx: Receiver<NamedEntry>,
+    ps: &Sender<ProgressState>,
+    errors: &mut ErrorCollector,
+    cfgmap: &cfg::ConfigMap,
+) -> Result_<()> {
     let mut cv = Vec::new();
     for (n, ne) in rx.into_iter().enumerate() {
         wrap_send(ps, ProgressState::Push(n, ne.0.clone()))?;
@@ -245,7 +279,8 @@ fn saving<S: EntrySaver>(saver: &mut S, rx: Receiver<NamedEntry>, ps: &Sender<Pr
 }
 
 fn wrap_send<T>(s: &Sender<T>, t: T) -> Result_<()> {
-    s.send(t).map_err(|_| anyhow::anyhow!("channel closed early"))
+    s.send(t)
+        .map_err(|_| anyhow::anyhow!("channel closed early"))
 }
 
 fn invalid_file_name<T>() -> Result_<T> {
