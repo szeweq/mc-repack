54550abb07baefa003a748738399e43b746566b9
Szewek <shevkpl@gmail.com>
2025-12-02 02:55:35 +0100

    Leaner file-in-jar copy

--- lib-core/src/min/jar.rs
+++ lib-core/src/min/jar.rs
@@ -2,11 +2,16 @@
 
 use std::io::{Cursor, Read, Write};
 
-use zip::{write::{FileOptions, SimpleFileOptions}, CompressionMethod, ZipArchive, ZipWriter};
+use zip::{
+    CompressionMethod, HasZipMetadata, ZipArchive, ZipWriter,
+    write::{FileOptions, SimpleFileOptions},
+};
 
-use crate::{cfg::{acfg, ConfigHolder}, entry::zip::compress_check};
 use super::Result_;
-
+use crate::{
+    cfg::{ConfigHolder, acfg},
+    entry::zip::compress_check,
+};
 
 acfg!(
     /// A JAR archive repacker that accepts [`JARConfig`].
@@ -16,23 +21,33 @@ acfg!(
 impl ConfigHolder<MinifierJAR> {
     pub(super) fn minify(&self, b: &[u8], vout: &mut Vec<u8>) -> Result_ {
         let mut zread = ZipArchive::new(Cursor::new(b))?;
-        let stored: SimpleFileOptions = FileOptions::default().compression_method(CompressionMethod::Stored);
-        let deflated: SimpleFileOptions = FileOptions::default().compression_method(CompressionMethod::Deflated).compression_level(Some(self.compress_level()));
+        let stored: SimpleFileOptions =
+            FileOptions::default().compression_method(CompressionMethod::Stored);
+        let deflated: SimpleFileOptions = FileOptions::default()
+            .compression_method(CompressionMethod::Deflated)
+            .compression_level(Some(self.compress_level()));
         let mut zwrite = ZipWriter::new(Cursor::new(vout));
         let mut v = Vec::new();
         for i in 0..zread.len() {
-            let Some(name) = zread.name_for_index(i).map(|s| s.to_string()) else { continue; };
-            if !self.keep_dirs && name.ends_with('/') { continue; }
+            let Some(is_dir) = zread.name_for_index(i).map(|s| s.ends_with('/')) else {
+                continue;
+            };
+            if !self.keep_dirs && is_dir {
+                continue;
+            }
 
             let mut zfile = zread.by_index(i)?;
             v.clear();
-            v.reserve(zfile.size() as usize);
+            v.reserve(zfile.get_metadata().uncompressed_size as usize);
             zfile.read_to_end(&mut v)?;
-            zwrite.start_file(name, if compress_check(&v, 24) {
-                deflated
-            } else {
-                stored
-            })?;
+            zwrite.start_file(
+                &zfile.get_metadata().file_name,
+                if compress_check(&v, 24) {
+                    deflated
+                } else {
+                    stored
+                },
+            )?;
             zwrite.write_all(&v)?;
         }
 
@@ -49,13 +64,15 @@ pub struct JARConfig {
 
     #[cfg(feature = "zip-zopfli")]
     /// Enables Zopfli compression (better, but slower)
-    pub use_zopfli: crate::cfg::CfgZopfli
+    pub use_zopfli: crate::cfg::CfgZopfli,
 }
 impl JARConfig {
     #[inline]
     #[cfg(feature = "zip-zopfli")]
     fn compress_level(&self) -> i64 {
-        self.use_zopfli.iter_count().map_or(9, |ic| 9 + ((u8::from(ic)) as i64))
+        self.use_zopfli
+            .iter_count()
+            .map_or(9, |ic| 9 + i64::from(ic.get()))
     }
 
     #[inline]
